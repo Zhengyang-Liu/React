0082236d8461928af376050d60d5cc1a2f4eb685
Zhengyang Liu <liuzhengyang183@gmail.com>
2018-12-19 23:07:36 +0800

    update map

--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -45,21 +45,19 @@ class Menu extends Component {
 
     render() {
 
-        const menu = this.state.dishes.map((dish) => {
-            return (
-                <div key={dish.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={dish.image} alt={dish.name}/>
-                        </Media>
-                        <Media body className="ml-5">
-                            <Media heading>{dish.name}</Media>
-                            <p>{dish.description}</p>
-                        </Media>
+        const menu = this.state.dishes.map(dish => 
+            <div key={dish.id} className="col-12 mt-5">
+                <Media tag="li">
+                    <Media left middle>
+                        <Media object src={dish.image} alt={dish.name}/>
                     </Media>
-                </div>
-            );
-        });
+                    <Media body className="ml-5">
+                        <Media heading>{dish.name}</Media>
+                        <p>{dish.description}</p>
+                    </Media>
+                </Media>
+            </div>
+        );
 
         return (
             <div className="container">
