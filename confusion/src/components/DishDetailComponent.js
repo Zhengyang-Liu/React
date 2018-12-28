dd1fdd02aacaf65e703575b163fa77d70722bf75
Zhengyang Liu <liuzhengyang183@gmail.com>
2018-12-28 22:58:18 +0800

    hw2

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({ dish }) {
     return (
@@ -33,12 +34,22 @@ const DishDetail = (props) => {
         return (
             <div class="container">
                 <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr></hr>
+                    </div>
+                </div>
+                <div className="row">
                     <div className="col-12 col-md-5 m-1">
                         <RenderDish dish={props.dish}></RenderDish>
                     </div>
                     <div className="col-12 col-md-5 m-1">
                         <h4>Comments</h4>
-                        <RenderComments dishComments={props.dish.comments}></RenderComments>
+                        <RenderComments dishComments={props.comments}></RenderComments>
                     </div>
                 </div>
             </div>
--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-
-function RenderMenuItem({ dish, onClick }) {
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+function RenderMenuItem({ dish }) {
     return (
-        <Card onClick={() => onClick(dish.id)}>
-            <CardImg width="100%" object src={dish.image} alt={dish.name} />
-            <CardImgOverlay>
-                <CardTitle>{dish.name}</CardTitle>
-            </CardImgOverlay>
+        <Card>
+            <Link to={`/menu/${dish.id}`} >
+                <CardImg width="100%" object src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
     );
 }
@@ -15,12 +17,22 @@ function RenderMenuItem({ dish, onClick }) {
 const Menu = (props) => {
     const menu = props.dishes.map(dish =>
         <div key={dish.id} className="col-12 col-md-5 m-1">
-            <RenderMenuItem dish={dish} onClick={props.onClick}></RenderMenuItem>
+            <RenderMenuItem dish={dish}></RenderMenuItem>
         </div>
     );
 
     return (
         <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='home'>Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Menu</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Menu</h3>
+                    <hr></hr>
+                </div>
+            </div>
             <div className="row">
                 {menu}
             </div>
