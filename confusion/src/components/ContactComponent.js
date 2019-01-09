074f46d63654e5bd07ca42d18e0fd63a0150c1c9
Zhengyang Liu <liuzhengyang183@gmail.com>
2019-01-09 20:37:43 +0800

    test

--- confusion/src/components/ContactComponent.js
+++ confusion/src/components/ContactComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem, Form, FormGroup, Label, Input, Col, Button, FormFeedback } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-class Contact extends Component {
+class Contact extends Component { 
     constructor(props) {
         super(props);
 
